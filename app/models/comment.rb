a65102ebc897c96326b3f8c8745d517918895b06
Eric Loudon <loudon.eric@gmail.com>
2019-10-28 16:58:17 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,4 @@ class Comment < ActiveRecord::Base
 
   accepts_nested_attributes_for :categories, :comments
 
-  def user_attributes=(user_attributes)
-    self.user = User.find_or_create_by(user_attributes)
-  end
 end
