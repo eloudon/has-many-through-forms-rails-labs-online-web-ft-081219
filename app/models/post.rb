28794d56b71fad450e38b7603907b407bdf9478b
Eric Loudon <loudon.eric@gmail.com>
2019-10-28 16:42:45 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,12 @@ class Post < ActiveRecord::Base
 
   accepts_nested_attributes_for :categories, reject_if: :all_blank
 
+  def categories_attributes=(category_attributes)
+  category_attributes.values.each do |category_attribute|
+    category = Category.find_or_create_by(category_attribute)
+    self.categories << category
+  end
+end
+
 
 end
